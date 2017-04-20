8653ae3d51ed91211a4ea508aa2512994272c0b0
Asia Kane and Nick Powell <nick@>
2017-04-20 10:41:29 -0700

    refactor with each index loop.

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,20 +1,34 @@
+# class Fixnum
+#   define_method(:coin) do
+#     coin_array =[]
+#     quarters = self./(25).floor()
+#     coin_array.push(quarters)
+#
+#     new_cents = self.%(25)
+#     dimes = new_cents./(10).floor()
+#     coin_array.push(dimes)
+#
+#     new_cents = new_cents.%(10)
+#     nickels = new_cents./(5).floor()
+#     coin_array.push(nickels)
+#
+#     pennies = new_cents.%(5)
+#     coin_array.push(pennies)
+#
+#     coin_array
+#   end
+# end
+
+
 class Fixnum
   define_method(:coin) do
     coin_array =[]
-    quarters = self./(25).floor()
-    coin_array.push(quarters)
-
-    new_cents = self.%(25)
-    dimes = new_cents./(10).floor()
-    coin_array.push(dimes)
-
-    new_cents = new_cents.%(10)
-    nickels = new_cents./(5).floor()
-    coin_array.push(nickels)
-
-    pennies = new_cents.%(5)
-    coin_array.push(pennies)
-
+    value_array = [25,10,5,1]
+    current_value = self
+    value_array.each_index() do |index|
+      coin_array.push(current_value./(value_array[index]).floor())
+      current_value = current_value.%(value_array[index])
+    end
     coin_array
   end
 end
